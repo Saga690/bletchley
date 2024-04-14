0245383f74c1a6529573274ae86d0ad02f2b6bc0
Saga690 <master.ayush.2003@gmail.com>
2024-04-14 14:54:42 +0530

    login & register ui changes

--- client/src/pages/Login/login.jsx
+++ client/src/pages/Login/login.jsx
@@ -5,26 +5,32 @@ import { FaLock } from "react-icons/fa";
 
 const Login = () => {
   return (
-    <div className='wrapper'>
-      <form action="">
-        <h1>Login</h1>
-        <div className="input-box">
-          <input type="email" placeholder='Email' required />
-          <FaUser className='icon' />
-        </div>
-        <div className="input-box">
-          <input type="password" placeholder='Password' required />
-          <FaLock className='icon' />
-        </div>
-        <div className="remember-forgot">
-          <label><input type="checkbox" />Remember Me</label>
-          <a href="#">Forgot Password?</a>
-        </div>
-        <button type='submit'>Login</button>
-        <div className="register-link">
-          <p>Don't have an account? <a href="/register">Register</a></p>
-        </div>
-      </form>
+    <div className="main">
+      <video autoPlay muted loop id="myVideo">
+        <source src="./src/assets/videobg1.mp4" type="video/mp4" />
+        Your browser does not support HTML5 video.
+      </video>
+      <div className='wrapper'>
+        <form action="">
+          <h1>Login</h1>
+          <div className="input-box">
+            <input type="email" placeholder='Email' required />
+            <FaUser className='icon' />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder='Password' required />
+            <FaLock className='icon' />
+          </div>
+          <div className="remember-forgot">
+            <label><input type="checkbox" />Remember Me</label>
+            <a href="#">Forgot Password?</a>
+          </div>
+          <button type='submit'>Login</button>
+          <div className="register-link">
+            <p>Don't have an account? <a href="/register">Register</a></p>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -5,30 +5,36 @@ import { FaLock } from "react-icons/fa";
 
 const Register = () => {
   return (
-    <div className='wrapper'>
-      <form action="">
-        <h1>Register</h1>
-        <div className="input-box">
-          <input type="text" placeholder='Username' required />
-          <FaUser className='icon' />
-        </div>
-        <div className="input-box">
-          <input type="email" placeholder='Email' required />
-          <FaUser className='icon' />
-        </div>
-        <div className="input-box">
-          <input type="password" placeholder='Password' required />
-          <FaLock className='icon' />
-        </div>
-        <div className="remember-forgot">
-          <label><input type="checkbox" />Remember Me</label>
-          {/* <a href="#">Forgot Password?</a> */}
-        </div>
-        <button type='submit'>Register</button>
-        <div className="register-link">
-          <p>Already have an account? <a href="/login">Sign In</a></p>
-        </div>
-      </form>
+    <div className='main'>
+      <video autoPlay muted loop id="myVideo">
+        <source src="./src/assets/videobg2.mp4" type="video/mp4" />
+        Your browser does not support HTML5 video.
+      </video>
+      <div className='wrapper'>
+        <form action="">
+          <h1>Register</h1>
+          <div className="input-box">
+            <input type="text" placeholder='Username' required />
+            <FaUser className='icon' />
+          </div>
+          <div className="input-box">
+            <input type="email" placeholder='Email' required />
+            <FaUser className='icon' />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder='Password' required />
+            <FaLock className='icon' />
+          </div>
+          <div className="remember-forgot">
+            <label><input type="checkbox" />Remember Me</label>
+            {/* <a href="#">Forgot Password?</a> */}
+          </div>
+          <button type='submit'>Register</button>
+          <div className="register-link">
+            <p>Already have an account? <a href="/login">Sign In</a></p>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
